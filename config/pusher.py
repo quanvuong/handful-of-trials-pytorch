87958b3223ace884ef2e54aa187f64e495aa3638
Quan Vuong <quan.vuong@nyu.edu>
2019-01-04 13:34:33 -0800

    add code for pusher env

---
+++ config/pusher.py
@@ -0,0 +1,218 @@
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+import numpy as np
+import tensorflow as tf
+import gym
+import torch
+from torch import nn as nn
+from torch.nn import functional as F
+
+from DotmapUtils import get_required_argument
+
+
+TORCH_DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+
+def swish(x):
+    return x * torch.sigmoid(x)
+
+
+def truncated_normal(size, std):
+
+    # Do not allow tf to use gpu memory unnecessarily
+    cfg = tf.ConfigProto()
+    cfg.gpu_options.allow_growth = True
+
+    sess = tf.Session(config=cfg)
+    val = sess.run(tf.truncated_normal(shape=size, stddev=std))
+
+    # Close the session and free resources
+    sess.close()
+
+    return torch.tensor(val, dtype=torch.float32)
+
+
+def get_affine_params(ensemble_size, in_features, out_features):
+
+    w = truncated_normal(size=(ensemble_size, in_features, out_features),
+                         std=1.0 / (2.0 * np.sqrt(in_features)))
+    w = nn.Parameter(w)
+
+    b = nn.Parameter(torch.zeros(ensemble_size, 1, out_features, dtype=torch.float32))
+
+    return w, b
+
+
+class PtModel(nn.Module):
+
+    def __init__(self, ensemble_size, in_features, out_features):
+        super().__init__()
+
+        self.num_nets = ensemble_size
+
+        self.in_features = in_features
+        self.out_features = out_features
+
+        self.lin0_w, self.lin0_b = get_affine_params(ensemble_size, in_features, 200)
+
+        self.lin1_w, self.lin1_b = get_affine_params(ensemble_size, 200, 200)
+
+        self.lin2_w, self.lin2_b = get_affine_params(ensemble_size, 200, 200)
+
+        self.lin3_w, self.lin3_b = get_affine_params(ensemble_size, 200, out_features)
+
+        self.inputs_mu = nn.Parameter(torch.zeros(in_features), requires_grad=False)
+        self.inputs_sigma = nn.Parameter(torch.zeros(in_features), requires_grad=False)
+
+        self.max_logvar = nn.Parameter(torch.ones(1, out_features // 2, dtype=torch.float32) / 2.0)
+        self.min_logvar = nn.Parameter(- torch.ones(1, out_features // 2, dtype=torch.float32) * 10.0)
+
+    def compute_decays(self):
+
+        lin0_decays = 0.00025 * (self.lin0_w ** 2).sum() / 2.0
+        lin1_decays = 0.0005 * (self.lin1_w ** 2).sum() / 2.0
+        lin2_decays = 0.0005 * (self.lin2_w ** 2).sum() / 2.0
+        lin3_decays = 0.00075 * (self.lin3_w ** 2).sum() / 2.0
+
+        return lin0_decays + lin1_decays + lin2_decays + lin3_decays
+
+    def fit_input_stats(self, data):
+
+        mu = np.mean(data, axis=0, keepdims=True)
+        sigma = np.std(data, axis=0, keepdims=True)
+        sigma[sigma < 1e-12] = 1.0
+
+        self.inputs_mu.data = torch.from_numpy(mu).to(TORCH_DEVICE).float()
+        self.inputs_sigma.data = torch.from_numpy(sigma).to(TORCH_DEVICE).float()
+
+    def forward(self, inputs, ret_logvar=False):
+
+        # Transform inputs
+        inputs = (inputs - self.inputs_mu) / self.inputs_sigma
+
+        inputs = inputs.matmul(self.lin0_w) + self.lin0_b
+        inputs = swish(inputs)
+
+        inputs = inputs.matmul(self.lin1_w) + self.lin1_b
+        inputs = swish(inputs)
+
+        inputs = inputs.matmul(self.lin2_w) + self.lin2_b
+        inputs = swish(inputs)
+
+        inputs = inputs.matmul(self.lin3_w) + self.lin3_b
+
+        mean = inputs[:, :, :self.out_features // 2]
+
+        logvar = inputs[:, :, self.out_features // 2:]
+        logvar = self.max_logvar - F.softplus(self.max_logvar - logvar)
+        logvar = self.min_logvar + F.softplus(logvar - self.min_logvar)
+
+        if ret_logvar:
+            return mean, logvar
+
+        return mean, torch.exp(logvar)
+
+
+class PusherConfigModule:
+    ENV_NAME = "MBRLPusher-v0"
+    TASK_HORIZON = 150
+    NTRAIN_ITERS = 100
+    NROLLOUTS_PER_ITER = 1
+    PLAN_HOR = 25
+    MODEL_IN, MODEL_OUT = 27, 20
+    GP_NINDUCING_POINTS = 200
+
+    def __init__(self):
+        self.ENV = gym.make(self.ENV_NAME)
+        self.NN_TRAIN_CFG = {"epochs": 5}
+        self.OPT_CFG = {
+            "Random": {
+                "popsize": 2500
+            },
+            "CEM": {
+                "popsize": 500,
+                "num_elites": 50,
+                "max_iters": 5,
+                "alpha": 0.1
+            }
+        }
+
+        # Keep track of the previous goal pos
+        # to determine if we should replace the goal pos on GPU
+        # to minimize communication overhead
+        self.prev_ac_goal_pos = None
+        self.goal_pos_gpu = None
+
+    @staticmethod
+    def obs_postproc(obs, pred):
+        return obs + pred
+
+    @staticmethod
+    def targ_proc(obs, next_obs):
+        return next_obs - obs
+
+    def obs_cost_fn(self, obs):
+        to_w, og_w = 0.5, 1.25
+        tip_pos, obj_pos, goal_pos = obs[:, 14:17], obs[:, 17:20], self.ENV.ac_goal_pos
+
+        assert isinstance(obs, torch.Tensor)
+
+        should_replace = False
+
+        # If there was a previous goal pos
+        # and the current goal pos is different from the previous goal post...
+        if self.prev_ac_goal_pos is not None and (self.prev_ac_goal_pos == goal_pos).all() is False:
+            # then we replace the goal pos on GPU
+            should_replace = True
+
+        # else if there is no current goal pos...
+        elif self.goal_pos_gpu is None:
+            # then we also move the goal pos to GPU
+            should_replace = True
+
+        if should_replace:
+            self.goal_pos_gpu = torch.from_numpy(goal_pos).float().to(TORCH_DEVICE)
+            self.prev_ac_goal_pos = goal_pos
+
+        tip_obj_dist = (tip_pos - obj_pos).abs().sum(dim=1)
+        obj_goal_dist = (self.goal_pos_gpu - obj_pos).abs().sum(dim=1)
+
+        return to_w * tip_obj_dist + og_w * obj_goal_dist
+
+        # if isinstance(obs, np.ndarray):
+        #     tip_obj_dist = np.sum(np.abs(tip_pos - obj_pos), axis=1)
+        #     obj_goal_dist = np.sum(np.abs(goal_pos - obj_pos), axis=1)
+        #     return to_w * tip_obj_dist + og_w * obj_goal_dist
+        # else:
+        #     tip_obj_dist = tf.reduce_sum(tf.abs(tip_pos - obj_pos), axis=1)
+        #     obj_goal_dist = tf.reduce_sum(tf.abs(goal_pos - obj_pos), axis=1)
+        #     return to_w * tip_obj_dist + og_w * obj_goal_dist
+
+    @staticmethod
+    def ac_cost_fn(acs):
+        return 0.1 * (acs ** 2).sum(dim=1)
+
+        # if isinstance(acs, np.ndarray):
+        #     return 0.1 * np.sum(np.square(acs), axis=1)
+        # else:
+        #     return 0.1 * tf.reduce_sum(tf.square(acs), axis=1)
+
+    def nn_constructor(self, model_init_cfg):
+        ensemble_size = get_required_argument(model_init_cfg, "num_nets", "Must provide ensemble size")
+
+        load_model = model_init_cfg.get("load_model", False)
+
+        assert load_model is False, 'Has yet to support loading model'
+
+        model = PtModel(ensemble_size,
+                        self.MODEL_IN, self.MODEL_OUT * 2).to(TORCH_DEVICE)
+        # * 2 because we output both the mean and the variance
+
+        model.optim = torch.optim.Adam(model.parameters(), lr=0.001)
+
+        return model
+
+
+CONFIG_MODULE = PusherConfigModule
--- env/__init__.py
+++ env/__init__.py
@@ -4,3 +4,8 @@ register(
     id='MBRLCartpole-v0',
     entry_point='env.cartpole:CartpoleEnv'
 )
+
+register(
+    id='MBRLPusher-v0',
+    entry_point='env.pusher:PusherEnv'
+)
