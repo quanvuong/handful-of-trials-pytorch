f9c0f52fe9f0ab484e2ab6012a9b325503e7ace8
Quan Vuong <quan.vuong@nyu.edu>
2019-01-04 13:39:51 -0800

    create shared utils file in config folder

--- config/pusher.py
+++ config/pusher.py
@@ -3,46 +3,15 @@ from __future__ import print_function
 from __future__ import absolute_import
 
 import numpy as np
-import tensorflow as tf
 import gym
 import torch
 from torch import nn as nn
 from torch.nn import functional as F
 
 from DotmapUtils import get_required_argument
-
+from config.utils import swish, get_affine_params
 
 TORCH_DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-
-
-def swish(x):
-    return x * torch.sigmoid(x)
-
-
-def truncated_normal(size, std):
-
-    # Do not allow tf to use gpu memory unnecessarily
-    cfg = tf.ConfigProto()
-    cfg.gpu_options.allow_growth = True
-
-    sess = tf.Session(config=cfg)
-    val = sess.run(tf.truncated_normal(shape=size, stddev=std))
-
-    # Close the session and free resources
-    sess.close()
-
-    return torch.tensor(val, dtype=torch.float32)
-
-
-def get_affine_params(ensemble_size, in_features, out_features):
-
-    w = truncated_normal(size=(ensemble_size, in_features, out_features),
-                         std=1.0 / (2.0 * np.sqrt(in_features)))
-    w = nn.Parameter(w)
-
-    b = nn.Parameter(torch.zeros(ensemble_size, 1, out_features, dtype=torch.float32))
-
-    return w, b
 
 
 class PtModel(nn.Module):
---
+++ config/utils.py
@@ -0,0 +1,34 @@
+import numpy as np
+import tensorflow as tf
+import torch
+from torch import nn as nn
+
+
+def swish(x):
+    return x * torch.sigmoid(x)
+
+
+def truncated_normal(size, std):
+
+    # Do not allow tf to use gpu memory unnecessarily
+    cfg = tf.ConfigProto()
+    cfg.gpu_options.allow_growth = True
+
+    sess = tf.Session(config=cfg)
+    val = sess.run(tf.truncated_normal(shape=size, stddev=std))
+
+    # Close the session and free resources
+    sess.close()
+
+    return torch.tensor(val, dtype=torch.float32)
+
+
+def get_affine_params(ensemble_size, in_features, out_features):
+
+    w = truncated_normal(size=(ensemble_size, in_features, out_features),
+                         std=1.0 / (2.0 * np.sqrt(in_features)))
+    w = nn.Parameter(w)
+
+    b = nn.Parameter(torch.zeros(ensemble_size, 1, out_features, dtype=torch.float32))
+
+    return w, b
